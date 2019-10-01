cd3dfce306c6954672926ede57a5318e2f5a6347
Kalamath <thecodedimage@gmail.com>
2019-09-30 21:37:58 -0700

    testing out questions div

--- public/javascript/ViewController.js
+++ public/javascript/ViewController.js
@@ -19,57 +19,65 @@ class ViewController {
         this.matchNameElement = $("#matchName");
         this.matchPhotoElement = $("#matchPhoto");
         this.percentageMatchElement = $("#percentageMatch");
+        this.questionsForSurvey = $("#surveyQuestions");
+        this.loginBTNElement = $("#loginBTN");
 
         this.selectElements = [];
 
         this.assignListeners();
         
-        this.assignQuestions();
+        // this.assignQuestions();
     }
 
-    assignQuestions() {
+    // assignQuestions() {
 
-        let count = 1;
+    //     let count = 1;
 
-        const classes = "formInput dropdownInput";
+    //     const classes = "formInput dropdownInput";
 
-        for (const question of surveyQuestions) {
+    //     for (const question of surveyQuestions) {
 
-            const newSelectElement = $("<select>").attr("id", "question" + count).addClass(classes).attr("required", "");
+    //         const newSelectElement = $("<select>").attr("id", "question" + count).addClass(classes).attr("required", "");
 
-            const newOptionElement = $("<option>").attr("value", "").text(question.question);
+    //         const newOptionElement = $("<option>").attr("value", "").text(question.question);
 
-            newSelectElement.append(newOptionElement);
+    //         newSelectElement.append(newOptionElement);
 
-            for (const option of surveyOptions) {
+    //         for (const option of surveyOptions) {
 
-                const newOptionElement = $("<option>").attr("value", option.value).html(option.text);
+    //             const newOptionElement = $("<option>").attr("value", option.value).html(option.text);
 
-                newSelectElement.append(newOptionElement);
-            }
+    //             newSelectElement.append(newOptionElement);
+    //         }
 
-            this.questionsWrapperElement.append(newSelectElement);
+    //         this.questionsWrapperElement.append(newSelectElement);
 
-            this.selectElements.push(newSelectElement);
+    //         this.selectElements.push(newSelectElement);
 
-            count++;
-        }
-    }
+    //         count++;
+    //     }
+    // }
 
     assignListeners() {
 
         this.startLoginBTNElement.one("click", () => {
+            console.log("Im here first")
+            this.login();
+        });
+
+        this.loginBTNElement.one("click", () => {
 
             this.startSurvey();
-        });
+        })
 
-        this.submitLoginBTNElement.click((event) => {
+        // this.submitLoginBTNElement.click((event) => {
 
-            this.submitSurvey(event);
-        });
+        //     this.startSurvey(event);
+        //     // this.submitSurvey(event);
+        // });
     }
 
-    startSurvey() {
+    login() {
 
         this.welcomeScreenElement.fadeTo(500, 0.0).promise().then(() => {
 
@@ -93,47 +101,59 @@ class ViewController {
         });
     }
 
-    submitSurvey(event) {
+    startSurvey() {
 
-        const name = this.nameInputElement.val().trim();
-        const photo = this.photoInputElement.val().trim();
-        const scores = [];
+        this.surveyScreenElement.fadeTo(500, 0.0).promise().then(() => {
 
-        for (const selectElement of this.selectElements) {
+            setTimeout(() => {
 
-            const score = selectElement.val().trim();
-
-            scores.push(score);
-        }
-
-        if (this.isInputValid(name, photo, scores)) {
-
-            event.preventDefault();
-
-            const newFriend =
-            {
-                name: name,
-                photo: photo,
-                scores: scores
-            };
-
-            $.post("/api/friends", newFriend, (friendMatch) => {
-
-                console.log(`Friend Match...\n   id    : ${friendMatch.id}\n   name  : ${friendMatch.name}\n   photo : ${friendMatch.photo}\n   scores: [${friendMatch.scores}]\n   match%: ${friendMatch.percentageMatch}\n\n`);
-
-                this.showFriendMatchModal(friendMatch.name, friendMatch.photo, friendMatch.percentageMatch);
-
-                this.clearInputFields();
-
-            }).fail((error) => {
-
-                console.log(`${error.responseJSON.message}  ${error.responseJSON.reason}\n\n`);
-
-                alert(`${error.responseJSON.message}  ${error.responseJSON.reason}\n\n`);
-
-            });
-        }
+                this.questionsForSurvey.fadeTo(1000, 1.0);
+            })
+        });
+        console.log('im here')
     }
+
+    // submitSurvey(event) {
+
+    //     const name = this.nameInputElement.val().trim();
+    //     const photo = this.photoInputElement.val().trim();
+    //     const scores = [];
+
+    //     for (const selectElement of this.selectElements) {
+
+    //         const score = selectElement.val().trim();
+
+    //         scores.push(score);
+    //     }
+
+    //     if (this.isInputValid(name, photo, scores)) {
+
+    //         event.preventDefault();
+
+    //         const newFriend =
+    //         {
+    //             name: name,
+    //             photo: photo,
+    //             scores: scores
+    //         };
+
+    //         $.post("/api/friends", newFriend, (friendMatch) => {
+
+    //             console.log(`Friend Match...\n   id    : ${friendMatch.id}\n   name  : ${friendMatch.name}\n   photo : ${friendMatch.photo}\n   scores: [${friendMatch.scores}]\n   match%: ${friendMatch.percentageMatch}\n\n`);
+
+    //             this.showFriendMatchModal(friendMatch.name, friendMatch.photo, friendMatch.percentageMatch);
+
+    //             this.clearInputFields();
+
+    //         }).fail((error) => {
+
+    //             console.log(`${error.responseJSON.message}  ${error.responseJSON.reason}\n\n`);
+
+    //             alert(`${error.responseJSON.message}  ${error.responseJSON.reason}\n\n`);
+
+    //         });
+    //     }
+    // }
 
     isInputValid(name, photo, scores) {
 
